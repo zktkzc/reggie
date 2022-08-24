9f3ba953e165f3ad6f9a238fd60d98edfd12a58c
tkzc00 <zqzkzy2420586708@qq.com>
2022-08-24 10:51:34 +0800

    套餐信息分页查询功能完成

--- src/main/java/com/reggie/controller/SetmealController.java
+++ src/main/java/com/reggie/controller/SetmealController.java
@@ -1,15 +1,21 @@
 package com.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.reggie.common.R;
 import com.reggie.dto.SetmealDto;
+import com.reggie.entity.Category;
+import com.reggie.entity.Setmeal;
+import com.reggie.service.CategoryService;
 import com.reggie.service.SetmealDishService;
 import com.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 套餐管理
@@ -27,6 +33,9 @@ public class SetmealController {
     @Autowired
     private SetmealDishService setmealDishService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     /**
      * 新增套餐
      *
@@ -42,4 +51,52 @@ public class SetmealController {
         return R.success("新增套餐成功");
     }
 
+    /**
+     * 套餐分页查询
+     *
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        // 分页构造器
+        Page<Setmeal> pageInfo = new Page<>(page, pageSize);
+        Page<SetmealDto> dtoPage = new Page<>();
+
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(name != null, Setmeal::getName, name);
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+
+        setmealService.page(pageInfo, queryWrapper);
+
+        // 对象拷贝
+        BeanUtils.copyProperties(pageInfo, dtoPage, "records");
+        List<Setmeal> records = pageInfo.getRecords();
+
+        List<SetmealDto> list = records.stream().map(item -> {
+
+            SetmealDto setmealDto = new SetmealDto();
+
+            // 对象拷贝
+            BeanUtils.copyProperties(item, setmealDto);
+
+            // 分类id
+            Long categoryId = item.getCategoryId();
+            // 根据id查询分类对象
+            Category category = categoryService.getById(categoryId);
+            if (category != null) {
+                // 分类名称
+                String categoryName = category.getName();
+                setmealDto.setCategoryName(categoryName);
+            }
+
+            return setmealDto;
+        }).collect(Collectors.toList());
+
+        dtoPage.setRecords(list);
+
+        return R.success(dtoPage);
+    }
 }
